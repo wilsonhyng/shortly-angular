bdd0d38e7015cbbef32bd5eb02752f5ef1979d66
Wilson Ng <Wilson@Wilsons-MacBook-Pro.local>
2016-12-03 12:36:44 -0800

    Implement shorten.html shorten link and display functionality

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,13 +2,19 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
-  $scope.link = {};
+  $scope.link = {}; // this is not storage
+  $scope.test = 'Testie McTester';
+  $scope.data = {}; 
 
   $scope.addLink = function() {
     Links.addLink($scope.link)
-    console.log('ERRRRRRORRRORORORORORORO')
-    .then(function (link) {
+    .then(function (respObj) {
+      console.log(respObj);
       // do something
+      $scope.data= respObj.data.baseUrl + '/' + respObj.data.code;
+
+
+
     })
     .catch(function (error) {
       console.error(error);
